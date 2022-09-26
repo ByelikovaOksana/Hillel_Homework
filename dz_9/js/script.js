bbe3ac0c4c438cf901af26da468c7273d6c1de27
Byelikova Oksana <byelikova.oksana@gmail.com>
2022-09-26 14:53:54 +0200

    Add files via upload

---
+++ dz_9/js/script.js
@@ -0,0 +1,45 @@
+const messageInput = document.querySelector('#msg');
+const listEl=document.querySelector('ul');
+const btn = document.querySelector('#myButton');
+
+btn.addEventListener('click', onBtnClick,);
+
+function onBtnClick() {
+    if (!validateForm()){
+        return;
+    }
+    generateMessage()
+    resetForm()
+};
+
+function validateForm(){
+    resetValidation();
+
+    if( messageInput.value ==='') {
+        messageInput.classList.add('invalid-input');
+        return false
+    }
+    return true
+}
+
+function resetValidation(){
+    messageInput.classList.remove('invalid-input')
+ }
+
+
+function generateMessage(){
+const liEl=document.createElement('li');
+const message=messageInput.value;
+liEl.textContent= message;
+listEl.append(liEl);
+liEl.addEventListener('click', () => {
+      liEl.classList.toggle('hover');
+})
+return liEl
+};
+
+function resetForm(){
+    messageInput.value='' 
+}
+
+
